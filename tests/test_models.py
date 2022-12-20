00912b80c6c0e7f54d140b653db432daabdec46e
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2022-12-20 08:13:02 -0500

    parametrize test for each model

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,20 +7,14 @@ from synnet.models.mlp import MLP
 
 CHECKPOINT_ICLR_DIR = "checkpoints/iclr"
 
-
-@pytest.fixture()
-def iclr_checkpoint_files() -> list[Path]:
-    return [
-        find_best_model_ckpt(Path(CHECKPOINT_ICLR_DIR) / model)
-        for model in "act rt1 rxn rt2".split()
-    ]
-
-
 @pytest.mark.skipif(
     not Path(CHECKPOINT_ICLR_DIR).exists(),  # assume if path exits, then all 4 files exist
     reason="ICLR checkpoints are not available",
 )
-def test_can_load_iclr_checkpoints(iclr_checkpoint_files):
-    models = [load_mlp_from_ckpt(file) for file in iclr_checkpoint_files]
-    assert len(models) == 4
-    assert all(isinstance(model, MLP) for model in models)
+@pytest.mark.parametrize("model", "act rt1 rxn rt2".split())
+def test_can_load_iclr_checkpoints(model: str):
+    ckpt_file = find_best_model_ckpt(Path(CHECKPOINT_ICLR_DIR) / model)
+    assert isinstance(ckpt_file, Path), f"Could not find checkpoint for {model}"
+
+    model = load_mlp_from_ckpt(ckpt_file)
+    assert isinstance(model, MLP)
