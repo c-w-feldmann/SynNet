b8c469e0eac4360a15e15821692da157b710ad7c
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2023-01-18 18:43:33 -0500

    adding some more tests

--- tests/test_syntree.py
+++ tests/test_syntree.py
@@ -1,8 +1,11 @@
 import json
+import logging
 
 import pytest
 
 from synnet.utils.data_utils import SyntheticTree
+
+logger = logging.getLogger(__name__)
 
 SYNTREE_FILE = "tests/assets/syntree-small.json"
 
@@ -90,18 +93,83 @@ def test_create_small_syntree(syntree_as_dict: dict):
     G = "C=C(C)C(=O)OCCNC(=O)N(c1ccc(C2=NNC(C3CCN(Cc4cnnn4CC(=O)NCCc4ccc(OCC)c(OCC)c4)CC3)=N2)cc1C)C1CC1"
     H = "C=C(C)C(=O)OCCNC(=O)N(c1ccc(-c2n[nH]c(C3CCN(Cc4cnnn4CC4=NCCc5cc(OCC)c(OCC)cc54)CC3)n2)cc1C)C1CC1"
     syntree = SyntheticTree()
+    assert syntree.depth == 0
+    assert syntree.root == None
+    assert syntree.get_state() == [], f"{syntree.get_state()=}"
     # 0: Add (bi)
     syntree.update(0, 12, A, B, C)
+    assert syntree.depth == 1
+    assert syntree.get_state() == [C], f"{syntree.get_state()=}"
     # 1: Add (bi)
     syntree.update(0, 47, D, E, F)
+    assert syntree.depth == 1
+    assert syntree.get_state() == [
+        F,
+        C,
+    ], f"{syntree.get_state()=}"  # the most recent root mol will be at index 0
     # 2: Merge (bi)
     syntree.update(2, 15, F, C, G)
+    assert syntree.depth == 2
+    assert syntree.get_state() == [G], f"{syntree.get_state()=}"
     # 3: Expand (uni)
     syntree.update(1, 49, G, None, H)
+    assert syntree.depth == 3
+    assert syntree.get_state() == [H], f"{syntree.get_state()=}"
     # 4: End
     syntree.update(3, None, None, None, None)
+    assert syntree.depth == 3
+    assert syntree.get_state() == [H], f"{syntree.get_state()=}"
+    assert syntree.root.smiles == H
     assert isinstance(syntree.to_dict(), dict)
     assert syntree.to_dict() == syntree_as_dict
+
+
+def test_syntree_with_repeating_bblock():
+    """Test creating a syntree with repeating bblock.
+
+    This resulted in a bug with 3 root mols until the fixed in 5ff1218.
+    """
+    syntree = SyntheticTree()
+    # Steps:
+    # 1: add uni
+    #   A + None -> B
+    # 2: add uni
+    #   C + None -> D
+    # 3: expand bi
+    #   D + E -> F
+    # 4: merge
+    #   F + B -> G
+    # 6: add uni
+    #   C + None -> D
+    # 7: expand bi
+    #  D + E -> F
+    _ACT = {act: i for i, act in enumerate("add expand merge end".split())}
+    syntree = SyntheticTree()
+    assert syntree.get_state() == []
+    # 1: add uni
+    syntree.update(_ACT["add"], 0, "A", None, "B")
+    assert syntree.get_state() == ["B"], f"{syntree.get_state()=}"
+    logger.debug(f"Iteration 1 | Syntree depth: {syntree.depth}, state: {syntree.get_state()}")
+    # 2: add uni
+    syntree.update(_ACT["add"], 1, "C", None, "D")
+    assert syntree.get_state() == ["D", "B"], f"{syntree.get_state()=}"
+    logger.debug(f"Iteration 2 | Syntree depth: {syntree.depth}, state: {syntree.get_state()}")
+    # 3: expand bi
+    syntree.update(_ACT["expand"], 2, "D", "E", "F")
+    assert syntree.get_state() == ["F", "B"], f"{syntree.get_state()=}"
+    logger.debug(f"Iteration 3 | Syntree depth: {syntree.depth}, state: {syntree.get_state()}")
+    # 4: merge
+    syntree.update(_ACT["merge"], 3, "F", "B", "G")
+    assert syntree.get_state() == ["G"], f"{syntree.get_state()=}"
+    logger.debug(f"Iteration 4 | Syntree depth: {syntree.depth}, state: {syntree.get_state()}")
+    # 5: add uni
+    syntree.update(_ACT["add"], 1, "C", None, "D")
+    assert syntree.get_state() == ["D", "G"], f"{syntree.get_state()=}"
+    logger.debug(f"Iteration 5 | Syntree depth: {syntree.depth}, state: {syntree.get_state()}")
+    # 6: expand bi
+    syntree.update(_ACT["expand"], 2, "D", "E", "F")
+    assert syntree.get_state() == ["F", "G"], f"{syntree.get_state()=}"
+    logger.debug(f"Iteration 6 | Syntree depth: {syntree.depth}, state: {syntree.get_state()}")
 
 
 def test_syntree_state():
