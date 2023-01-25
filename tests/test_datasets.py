4c2947b0d8aedf7618e0262941ee84f4797e3cf0
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2023-01-25 17:16:24 -0500

    update test

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -19,10 +19,6 @@ def test_chop_syntree(syntree_simplified: SyntheticTree):
     assert isinstance(syntree_simplified, SyntheticTree)
     chopped = ActSyntreeDataset.chop_syntree(syntree_simplified)
     ref_chopped = [
-        {"target": 0, "state": ("n8", None, None), "num_action": 0},
-        {"target": 0, "state": ("n8", "n3", None), "num_action": 1},
-        {"target": 2, "state": ("n8", "n6", "n3"), "num_action": 2},
-        {"target": 1, "state": ("n8", "n7", None), "num_action": 3},
-        {"target": 3, "state": ("n8", "n8", None), "num_action": 4},
-    ]
+    {'num_action': 0,  'target': 0,  'state': ('n8', None, None),  'reaction_id': 12,  'reactant_1': 'n1',  'reactant_2': 'n2'}, {'num_action': 1,  'target': 0,  'state': ('n8', 'n3', None),  'reaction_id': 47,  'reactant_1': 'n4',  'reactant_2': 'n5'}, {'num_action': 2,  'target': 2,  'state': ('n8', 'n6', 'n3'),  'reaction_id': 15,  'reactant_1': 'n6',  'reactant_2': 'n3'}, {'num_action': 3,  'target': 1,  'state': ('n8', 'n7', None),  'reaction_id': 49,  'reactant_1': 'n7',  'reactant_2': None}, {'num_action': 4,  'target': 3,  'state': ('n8', 'n8', None),  'reaction_id': None,  'reactant_1': None,  'reactant_2': None},
+    ]  # fmt: skip
     assert chopped == ref_chopped
