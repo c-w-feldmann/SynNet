d47ff756f45277ade086f43cd97852f03933271c
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2023-01-28 16:27:46 -0500

    replace dummy smiles/rxns with chars + numbers

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -3,13 +3,13 @@ from pathlib import Path
 
 import pytest
 
-from synnet.datasets import ActSyntreeDataset
+from synnet.datasets import SynTreeChopper
 from synnet.utils.data_utils import SyntheticTree, SyntheticTreeSet
 
 
 @pytest.fixture
 def syntree_simplified():
-    """Load a simplified syntree where SMILES are replaced with "n<int>"."""
+    """Load a simplified syntree where SMILES are replaced with chars A,B,C etc."""
     return SyntheticTree.from_dict(
         dict(json.loads(Path("tests/assets/syntree-small-simple.json").read_text()))
     )
@@ -17,8 +17,8 @@ def syntree_simplified():
 
 def test_chop_syntree(syntree_simplified: SyntheticTree):
     assert isinstance(syntree_simplified, SyntheticTree)
-    chopped = ActSyntreeDataset.chop_syntree(syntree_simplified)
+    chopped = SynTreeChopper.chop_syntree(syntree_simplified)
     ref_chopped = [
-    {'num_action': 0,  'target': 0,  'state': ('n8', None, None),  'reaction_id': 12,  'reactant_1': 'n1',  'reactant_2': 'n2'}, {'num_action': 1,  'target': 0,  'state': ('n8', 'n3', None),  'reaction_id': 47,  'reactant_1': 'n4',  'reactant_2': 'n5'}, {'num_action': 2,  'target': 2,  'state': ('n8', 'n6', 'n3'),  'reaction_id': 15,  'reactant_1': 'n6',  'reactant_2': 'n3'}, {'num_action': 3,  'target': 1,  'state': ('n8', 'n7', None),  'reaction_id': 49,  'reactant_1': 'n7',  'reactant_2': None}, {'num_action': 4,  'target': 3,  'state': ('n8', 'n8', None),  'reaction_id': None,  'reactant_1': None,  'reactant_2': None},
+    {'num_action': 0,  'target': 0,  'state': ('H', None, None),  'reaction_id': "r1",  'reactant_1': 'A',  'reactant_2': 'B'}, {'num_action': 1,  'target': 0,  'state': ('H', 'C', None),  'reaction_id': "r2",  'reactant_1': 'D',  'reactant_2': 'E'}, {'num_action': 2,  'target': 2,  'state': ('H', 'F', 'C'),  'reaction_id': "r3",  'reactant_1': 'F',  'reactant_2': 'C'}, {'num_action': 3,  'target': 1,  'state': ('H', 'G', None),  'reaction_id': "r4",  'reactant_1': 'G',  'reactant_2': None}, {'num_action': 4,  'target': 3,  'state': ('H', 'H', None),  'reaction_id': None,  'reactant_1': None,  'reactant_2': None},
     ]  # fmt: skip
     assert chopped == ref_chopped
