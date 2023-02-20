1484d9f72f688aa59d9f56875ff69bbd471cb687
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2023-02-20 14:24:32 -0500

    add default config: split ratio + num rxn tmplts

--- src/synnet/config.py
+++ src/synnet/config.py
@@ -7,3 +7,12 @@ DEFAULT_MAX_PROCESSES = 31
 MAX_PROCESSES = min(
     int(os.environ.get("SLURM_CPUS_PER_TASK", DEFAULT_MAX_PROCESSES)), multiprocessing.cpu_count()
 )
+
+
+# Default split ratio train/val/test
+SPLIT_RATIO = [0.6, 0.2, 0.2]
+
+# Number of reaction templates
+# (used for 1-hot-encoding of reaction templates)
+DEFAULT_NUM_RXN_TEMPLATES = 91
+NUM_RXN_TEMPLATES = os.environ.get("SYNNET_NUM_RXN_TEMPLATES", DEFAULT_NUM_RXN_TEMPLATES)
