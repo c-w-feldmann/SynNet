acbca9ba31a50dd2a196c03fbaad22cde481317a
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2025-03-21 13:33:43 +0100

    remove pass

--- src/synnet/utils/synnet_exceptions.py
+++ src/synnet/utils/synnet_exceptions.py
@@ -1,16 +1,10 @@
 class FailedReconstructionError(Exception):
     """Raised when the no valid reactant ist found."""
 
-    pass
-
 
 class NoSuitableReactantError(FailedReconstructionError):
     """Raised when the no valid reactant ist found."""
 
-    pass
-
 
 class StateEmbeddingError(FailedReconstructionError):
     """Raise when State Embedding failed"""
-
-    pass
