0aa1f32c7718cf1febf6d32e8aaf162ec4c8e447
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2025-03-21 16:57:15 +0100

    rewrite to unittests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,36 +1,26 @@
+"""Unittest for the datasets module."""
+
 import json
+import unittest
 from pathlib import Path
 
-import pytest
-
-from synnet.datasets import ActSyntreeDataset
+from synnet.datasets import SyntreeDataset
 from synnet.utils.data_utils import SyntheticTree
 
-base_dir = Path(__file__).parent.absolute()
 
+class TestSyntreeDataset(unittest.TestCase):
+    """Unit test for the SyntheticTree dataset."""
 
-@pytest.fixture
-def syntree_simplified() -> SyntheticTree:
-    """Load a simplified syntree where SMILES are replaced with "n<int>"."""
-    return SyntheticTree.from_dict(
-        dict(
-            json.load(
-                open(
-                    base_dir / "assets/syntree-small-simple.json", "r", encoding="utf-8"
-                )
-            )
-        )
-    )
+    def setUp(self) -> None:
+        """Set up the test."""
+        self.base_dir = Path(__file__).parent.absolute()
+        self.syntree_file = self.base_dir / "assets/syntree-small-simple.json"
 
-
-def test_chop_syntree(syntree_simplified: SyntheticTree) -> None:
-    assert isinstance(syntree_simplified, SyntheticTree)
-    chopped = ActSyntreeDataset.chop_syntree(syntree_simplified)
-    ref_chopped = [
-        {"target": 0, "state": ("n8", None, None), "num_action": 0},
-        {"target": 0, "state": ("n8", "n3", None), "num_action": 1},
-        {"target": 2, "state": ("n8", "n6", "n3"), "num_action": 2},
-        {"target": 1, "state": ("n8", "n7", None), "num_action": 3},
-        {"target": 3, "state": ("n8", "n8", None), "num_action": 4},
-    ]
-    assert chopped == ref_chopped
+    def test_loading(self) -> None:
+        """Test loading syntree from a file."""
+        if not self.syntree_file.exists():
+            self.skipTest("Syntree file not found.")
+        with open(self.syntree_file, "rt", encoding="UTF-8") as f:
+            syntree_dict = json.load(f)
+        syntree = SyntheticTree.from_dict(syntree_dict)
+        SyntreeDataset(dataset=[syntree])
