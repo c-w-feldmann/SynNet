b03fe8f8a14771db1a0bc5dc17a3ae52e3636c10
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2025-03-21 14:12:05 +0100

    switch to unittest

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,23 +1,24 @@
-from pathlib import Path
+"""Unit tests for the models module."""
 
-import pytest
+import unittest
+from pathlib import Path
 
 from synnet.models.common import find_best_model_ckpt, load_mlp_from_ckpt
 from synnet.models.mlp import MLP
 
-CHECKPOINT_ICLR_DIR = "checkpoints/iclr"
+CHECKPOINT_ICLR_DIR = Path("checkpoints/iclr")
 
 
-@pytest.mark.skipif(
-    not Path(
-        CHECKPOINT_ICLR_DIR
-    ).exists(),  # assume if path exits, then all 4 files exist
-    reason="ICLR checkpoints are not available",
-)
-@pytest.mark.parametrize("model", "act rt1 rxn rt2".split())
-def test_can_load_iclr_checkpoints(model: str) -> None:
-    ckpt_file = find_best_model_ckpt(Path(CHECKPOINT_ICLR_DIR) / model)
-    assert isinstance(ckpt_file, Path), f"Could not find checkpoint for {model}"
+class TestModels(unittest.TestCase):
+    """Unit tests for models."""
 
-    model = load_mlp_from_ckpt(ckpt_file)
-    assert isinstance(model, MLP)
+    @unittest.skipIf(
+        not CHECKPOINT_ICLR_DIR.exists(), "Checkpoint directory does not exist"
+    )
+    def test_can_load_iclr_checkpoints(self) -> None:
+        """Test if we can load all ICLR checkpoints."""
+        for model in "act rt1 rxn rt2".split():
+            ckpt_file = find_best_model_ckpt(Path(CHECKPOINT_ICLR_DIR) / model)
+
+            model = load_mlp_from_ckpt(ckpt_file)
+            self.assertIsInstance(model, MLP)
